e23bc9b89523c8b37c33d5df4393ad70b7d5edd3
jlugogar - Jose Lugo <jalugogarcia@gmail.com>
2017-09-05 08:08:23 -0400

    Adding fictional data source.

---
+++ DataAccessLayer/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace DataAccessLayer
+{
+    public class Person
+    {
+        private string id;
+        private string firstName;
+        private string lastName;
+        private string address;      
+
+        public Person(string id, string fn, string ln, string addrs)
+        {
+            this.id = id;
+            firstName = fn;
+            lastName = ln;
+            address = addrs;
+        }
+
+        public string Id { get { return id; }
+        }
+
+        public string FirstName { get { return firstName; }
+        }
+
+        public string LastName { get { return lastName; }
+        }
+
+        public string Address { get { return address; }
+        }
+
+    }
+}
